dd941c47fd5959fdc4f79e8bd8f3996d0771cb08
Lennart Blom <registrierungen@lennart-blom.de>
2017-11-05 13:03:24 +0100

    Clean up helloWorld function and use correct annotation

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,23 +1,14 @@
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class HelloWorld {
     @RequestMapping("/")
     String home(){
-        List<String> myStuffToPrint = new ArrayList<String>();
-        int counter = 0;
-        while(counter < 10){
-            counter++;
-            myStuffToPrint.add("A bike part with id <"+ counter+">\n<br>");
-        }
-        return "Hello World @ bike_mechanics\n<br>" + myStuffToPrint.toString();
+        return "Hello World @ bike_mechanics";
     }
 
     public static void main(String[] args) throws Exception{
