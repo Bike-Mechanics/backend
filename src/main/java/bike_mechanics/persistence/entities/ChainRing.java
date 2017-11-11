b2d5ba3c80c8695bf60575f536082b94307fa80a
heikoelberg <heikoelberg@web.de>
2017-11-11 22:37:32 +0100

    Fixed Packagedeclarations

--- src/main/java/bike_mechanics/persistence/entities/ChainRing.java
+++ src/main/java/bike_mechanics/persistence/entities/ChainRing.java
@@ -1,8 +1,10 @@
-package bike_mechanics.persistence.base_entities;
+package bike_mechanics.persistence.entities;
 
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+
+import bike_mechanics.persistence.base_entities.BikeComponent;
 
 @Entity
 public class ChainRing extends BikeComponent implements Serializable {
