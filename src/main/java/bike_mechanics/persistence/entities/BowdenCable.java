c1e58b63f335fa262743c32d701f27b6126df5af
heikoelberg <heikoelberg@web.de>
2017-11-11 22:35:55 +0100

    Added new SubEntities for BikeComponent

---
+++ src/main/java/bike_mechanics/persistence/entities/BowdenCable.java
@@ -0,0 +1,9 @@
+package bike_mechanics.persistence.base_entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class BowdenCable extends BikeComponent implements Serializable {
+}
