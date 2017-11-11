c31bfa574811bb8a810325445fb0f69e77ccb9f1
Lennart Blom <registrierungen@lennart-blom.de>
2017-11-11 18:57:27 +0100

    Fix annotations

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,14 +1,17 @@
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication
+@EnableAutoConfiguration
 public class HelloWorld {
-    @RequestMapping("/")
-    String home(){
-        return "Hello World @ bike_mechanics";
+    @RequestMapping(value="/{factor}", method = RequestMethod.GET)
+    String home(@PathVariable("factor") int factor){
+
+        return "Hello World @ bike_mechanics " + factor;
     }
 
     public static void main(String[] args) throws Exception{
