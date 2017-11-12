98a3876704b8183ce414157dd597b782773f7227
Lennart Blom <registrierungen@lennart-blom.de>
2017-11-12 19:41:37 +0100

    Fix generated id annotation

--- src/main/java/bike_mechanics/persistence/base_entities/BikeComponent.java
+++ src/main/java/bike_mechanics/persistence/base_entities/BikeComponent.java
@@ -1,8 +1,10 @@
 package bike_mechanics.persistence.base_entities;
 
 import bike_mechanics.persistence.enums.ReplacementStatus;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import java.io.Serializable;
@@ -13,7 +15,10 @@ import java.sql.Date;
 public abstract class BikeComponent implements Serializable {
 
     @Id
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
     long id;
+
     float milage;
     Date assemblyDate;
     ReplacementStatus status;
