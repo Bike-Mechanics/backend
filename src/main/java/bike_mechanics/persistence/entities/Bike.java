ff7bcdf85dc1ad48608b5dea3996d233efb4dd65
Lennart Blom <registrierungen@lennart-blom.de>
2017-11-12 19:42:20 +0100

    Optimize imports

--- src/main/java/bike_mechanics/persistence/entities/Bike.java
+++ src/main/java/bike_mechanics/persistence/entities/Bike.java
@@ -1,15 +1,23 @@
 package bike_mechanics.persistence.entities;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Bike implements Serializable {
 	
 	@Id
+	@GeneratedValue(generator="increment")
+	@GenericGenerator(name="increment", strategy = "increment")
+	Long id;
+
 	String stravaGearId;
 	String stravaName;
 
@@ -33,4 +41,23 @@ public class Bike implements Serializable {
 
 	@OneToMany
 	List<BowdenCable> bowdenCables;
+
+	public Bike(){
+		this.chains = new ArrayList<Chain>();
+	}
+
+	public Bike(String pStravaGearId, String pStravaName){
+		this();
+		this.stravaGearId = pStravaGearId;
+		this.stravaName = pStravaName;
+
+	}
+
+	public void addChain(Chain pChain){
+		this.chains.add(pChain);
+	}
+
+	public List<Chain> getChains(){
+		return this.chains;
+	}
 }
--- src/main/java/bike_mechanics/persistence/entities/BowdenCable.java
+++ src/main/java/bike_mechanics/persistence/entities/BowdenCable.java
@@ -1,10 +1,9 @@
 package bike_mechanics.persistence.entities;
 
-import java.io.Serializable;
+import bike_mechanics.persistence.base_entities.BikeComponent;
 
 import javax.persistence.Entity;
-
-import bike_mechanics.persistence.base_entities.BikeComponent;
+import java.io.Serializable;
 
 @Entity
 public class BowdenCable extends BikeComponent implements Serializable {
