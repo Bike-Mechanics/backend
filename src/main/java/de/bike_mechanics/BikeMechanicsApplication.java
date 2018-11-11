78b9e14b6b47774ae2133765007c1bfa903b11b0
Lennart Blom <registrierungen@lennart-blom.de>
2018-11-11 11:02:35 +0100

    Rename application properly

--- src/main/java/de/bike_mechanics/BikeMechanicsApplication.java
+++ src/main/java/de/bike_mechanics/BikeMechanicsApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class BikeMechanicsApplication {
     public static void main(String[] args) throws Exception{
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(BikeMechanicsApplication.class, args);
     }
 }
