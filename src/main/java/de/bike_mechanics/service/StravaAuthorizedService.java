2de3718ec4139688a67a81d58abcb99636b889d6
Lennart Blom <registrierungen@lennart-blom.de>
2018-11-11 11:04:28 +0100

    Clean up api abstraction
    - APIService delegates the authorized API calls to the StravaAuthorizedService
    - StravaAuthorizedService does not know anything about the bike-mechanics entities and just focuses on the API call

---
+++ src/main/java/de/bike_mechanics/service/StravaAuthorizedService.java
@@ -0,0 +1,45 @@
+package de.bike_mechanics.service;
+
+import javastrava.api.v3.auth.AuthorisationService;
+import javastrava.api.v3.auth.impl.retrofit.AuthorisationServiceImpl;
+import javastrava.api.v3.auth.model.Token;
+import javastrava.api.v3.model.StravaActivity;
+import javastrava.api.v3.service.Strava;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Slf4j
+public class StravaAuthorizedService {
+
+    private Strava stravaAPI;
+
+    public StravaAuthorizedService(@Value("${strava.client_id}") int clientId,
+                                   @Value("${strava.client_secret}") String clientSecret,
+                                   @Value("${strava.authorization_code}") String code) {
+
+        Token authorizationToken = this.authorizeForStravaAPI(clientId, clientSecret, code);
+        this.stravaAPI = new Strava(authorizationToken);
+    }
+
+
+    private Token authorizeForStravaAPI(int clientId, String clientSecret, String code) {
+        AuthorisationService service = new AuthorisationServiceImpl();
+        log.info("Authorization with: " + clientId + "" + clientSecret + "" + code);
+        return service.tokenExchange(clientId, clientSecret, code);
+    }
+
+    StravaActivity getActivity(int activityId){
+        return this.stravaAPI.getActivity(activityId);
+    }
+
+    List<StravaActivity> listAuthenticatedAthleteActivities(LocalDateTime before, LocalDateTime after){
+        return this.stravaAPI.listAuthenticatedAthleteActivities(before, after);
+    }
+
+
+}
