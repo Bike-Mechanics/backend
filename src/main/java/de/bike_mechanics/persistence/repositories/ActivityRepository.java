d403b2c764217138994f0078bdf23ef7da8a335b
Lennart Blom <registrierungen@lennart-blom.de>
2018-11-11 11:02:12 +0100

    Define persistence repositories

--- src/main/java/de/bike_mechanics/persistence/repositories/ActivityRepository.java
+++ src/main/java/de/bike_mechanics/persistence/repositories/ActivityRepository.java
@@ -1,9 +1,11 @@
 package de.bike_mechanics.persistence.repositories;
 
-import de.bike_mechanics.persistence.entities.Bike;
+import de.bike_mechanics.persistence.entities.Activity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BikeRepository extends CrudRepository<Bike, Long>{
+public interface ActivityRepository extends CrudRepository<Activity, Long> {
+
 }
+
